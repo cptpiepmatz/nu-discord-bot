b9081f163bb8dc97444aa007e779fab806be2d4d
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-03-15 22:45:15 +0100

    added middleware for discord endpoint

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,28 @@
-use axum::{Json, Router, response::IntoResponse, routing::get};
+use std::{sync::OnceLock, usize};
+
+use anyhow::{Context, anyhow};
+use axum::{
+    Json, Router,
+    body::{Body, to_bytes},
+    extract::Request,
+    http::StatusCode,
+    middleware::Next,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+};
+use ed25519_dalek::{Signature, Verifier, VerifyingKey};
+use hex_literal::hex;
 use serde_json::json;
+use twilight_model::{
+    application::interaction::Interaction,
+    id::{Id, marker::ApplicationMarker},
+};
+
+const APPLICATION_ID: Id<ApplicationMarker> = Id::new(1350440050927865878);
+const PUBLIC_KEY: [u8; 32] =
+    hex!("cc5d283a4aa8d726bcbd7938bfccda57984568d5e7b8546ac963a7cfd2fa0b5e");
+
+static VERIFY_KEY: OnceLock<VerifyingKey> = OnceLock::new();
 
 async fn version() -> impl IntoResponse {
     Json(json!({
@@ -8,11 +31,56 @@ async fn version() -> impl IntoResponse {
     }))
 }
 
+async fn discord(payload: Json<Interaction>) -> impl IntoResponse {
+    "" // TODO
+}
+
+async fn validate_security_headers(request: Request, next: Next) -> Result<Response, StatusCode> {
+    let (parts, body) = request.into_parts();
+    let headers = &parts.headers;
+
+    let verify_key = VERIFY_KEY.get().ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let signature = headers
+        .get("X-Signature-Ed25519")
+        .ok_or(StatusCode::BAD_REQUEST)?;
+    let signature = signature.to_str().map_err(|_| StatusCode::BAD_REQUEST)?;
+    let signature = hex::decode(signature).map_err(|_| StatusCode::BAD_REQUEST)?;
+    let signature = <[u8; 64]>::try_from(signature).map_err(|_| StatusCode::BAD_REQUEST)?;
+    let signature = Signature::from_bytes(&signature);
+
+    let timestamp = headers
+        .get("X-Signature-Timestamp")
+        .ok_or(StatusCode::BAD_REQUEST)?;
+    let timestamp = timestamp.to_str().map_err(|_| StatusCode::BAD_REQUEST)?;
+    let body = to_bytes(body, usize::MAX)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let body = String::from_utf8(body.to_vec()).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let msg = format!("{timestamp}{body}");
+
+    if verify_key.verify(msg.as_bytes(), &signature).is_ok() {
+        let body = Body::from(body);
+        let request = Request::from_parts(parts, body);
+        return Ok(next.run(request).await);
+    }
+
+    Err(StatusCode::UNAUTHORIZED)
+}
+
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
+    VERIFY_KEY
+        .set(VerifyingKey::from_bytes(&PUBLIC_KEY).context("invalid verifying key")?)
+        .map_err(|_| anyhow!("verify key somehow already set"))?;
+
     let router = Router::new()
         .route("/", get(version))
-        .route("/version", get(version));
+        .route("/version", get(version))
+        .route(
+            "/discord",
+            post(discord).layer(axum::middleware::from_fn(validate_security_headers)),
+        );
 
     Ok(router.into())
 }
