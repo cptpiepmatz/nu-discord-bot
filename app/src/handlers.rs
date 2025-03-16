5865fef83808a854408e82ecadede76dbfaa765c
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-03-16 15:30:30 +0100

    handle ping requests

--- app/src/handlers.rs
+++ app/src/handlers.rs
@@ -1,35 +1,38 @@
-use axum::{Json, http::StatusCode, response::Response};
-use twilight_model::application::interaction::{Interaction, InteractionType};
+use axum::{Json, http::StatusCode};
+use twilight_model::{
+    application::interaction::{Interaction, InteractionType},
+    http::interaction::{InteractionResponse, InteractionResponseType},
+};
 
-pub async fn discord(interaction: Json<Interaction>) -> Result<Response, StatusCode> {
+pub async fn discord(
+    interaction: Json<Interaction>,
+) -> Result<Json<InteractionResponse>, StatusCode> {
     match interaction.kind {
         InteractionType::Ping => handle_ping().await,
         InteractionType::ApplicationCommand => handle_application_command().await,
         InteractionType::MessageComponent => handle_message_component().await,
-        InteractionType::ApplicationCommandAutocomplete => {
-            handle_application_command_autocomplete().await
-        }
         InteractionType::ModalSubmit => handle_modal_submit().await,
-        _ => Err(StatusCode::UNPROCESSABLE_ENTITY),
+        InteractionType::ApplicationCommandAutocomplete | _ => {
+            Err(StatusCode::UNPROCESSABLE_ENTITY)
+        }
     }
 }
 
-async fn handle_ping() -> Result<Response, StatusCode> {
+async fn handle_ping() -> Result<Json<InteractionResponse>, StatusCode> {
+    Ok(InteractionResponse {
+        kind: InteractionResponseType::Pong,
+        data: None,
+    }.into())
+}
+
+async fn handle_application_command() -> Result<Json<InteractionResponse>, StatusCode> {
     todo!()
 }
 
-async fn handle_application_command() -> Result<Response, StatusCode> {
+async fn handle_message_component() -> Result<Json<InteractionResponse>, StatusCode> {
     todo!()
 }
 
-async fn handle_message_component() -> Result<Response, StatusCode> {
-    todo!()
-}
-
-async fn handle_application_command_autocomplete() -> Result<Response, StatusCode> {
-    Err(StatusCode::UNPROCESSABLE_ENTITY)
-}
-
-async fn handle_modal_submit() -> Result<Response, StatusCode> {
+async fn handle_modal_submit() -> Result<Json<InteractionResponse>, StatusCode> {
     todo!()
 }
